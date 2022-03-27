be5d47162961491cb7772ba0aa0628b7a7af560b
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2022-03-27 22:20:54 +0300

    Add project files.

---
+++ cachePractice/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using cachePractice.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cachePractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        const string cacheKey = "productKey";
+        private readonly IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product{Name="IPhone",Stock=100,Price=14000},
+                new Product{Name="Led TV",Stock=743,Price=25000},
+                new Product{Name="Washing Machine",Stock=655,Price=5000},
+                new Product{Name="Bed",Stock=450,Price=4000}
+            };
+            if (!_memoryCache.TryGetValue(cacheKey, out products))
+            {
+                var cacheExpireOptions = new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpiration = DateTime.Now.AddMinutes(10),
+                    Priority = CacheItemPriority.Normal
+                };
+                _memoryCache.Set(cacheKey, products, cacheExpireOptions);
+            }
+            return products;
+        }
+        [HttpGet]
+        public ActionResult DeleteCache()
+        {
+            _memoryCache.Remove(cacheKey);
+            return View();
+        }
+    }
+}
---
+++ cachePractice/Entity/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cachePractice.Entity
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal Stock { get; set; }
+
+    }
+}
